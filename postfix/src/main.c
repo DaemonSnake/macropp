b8517316e7c4239a49de9cf82f56ecf552ce2485
Bastien Penavayre <bastienPenava@gmail.com>
2016-11-29 11:23:29 -0500

    Adds details to valgrind rule

--- postfix/src/main.c
+++ postfix/src/main.c
@@ -2,20 +2,21 @@
 #include "inc.h"
 #include <mcheck.h>
 
-__attribute__((constructor))
-static void mem()
-{
-    mtrace();
-}
-
-int main()
+void loop()
 {
     buffer buf = new(0, 1);
 
     while (look_for(buf, "[@", NULL, true, PROCCESS))
         handle_arguments(buf);
     delete(buf);
+}
+
+int main()
+{
+    /* mtrace(); */
+    loop();
     clean_all_macros();
+    /* muntrace(); */
     return 0;
 }
 
