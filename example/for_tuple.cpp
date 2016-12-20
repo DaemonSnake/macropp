b8879fcac0abd45a20c61f269daeb366c97edb0a
Bastien Penavayre <bastienPenava@gmail.com>
2016-12-20 05:14:18 -0500

    Update examples

--- example/for_tuple.cpp
+++ example/for_tuple.cpp
@@ -4,56 +4,38 @@
 #include <typeinfo>
 
 enum class for_tuple_keys : int {
-    nothing = 0,
-    break_,
-    //return_ in future
-    continue_
+    continue_ = 0,
+    break_
 };
 
 template <size_t size>
-class VarargsIterator
+struct TupleIterator
 {
-    template <size_t index, bool isOk = (index < size)>
-    struct Iterator
-    {
-        static auto turn(auto t, auto func) {
-            auto ret = func(std::get<index>(t));
-            if (ret == for_tuple_keys::break_)
-                return ret;
-            return Iterator<index + 1>::turn(t, func);
-        }
-    };
-
-    template <size_t index>
-    struct Iterator<index, false>
-    {
-        static auto turn(auto t, auto func) {
-            return for_tuple_keys::nothing;
-        }
-    };
-
-public:
-    static auto iterate(auto holder, auto func) {
-        return Iterator<0>::turn(holder, func);
+    template <class T, class V, size_t index = 0>
+    static auto iterate(T holder, V func) {
+        if constexpr (index < size)
+         if (auto ret  = func(std::get<index>(holder)); ret != for_tuple_keys::break_)
+               return iterate<T, V, index + 1>(holder, func);
+        return for_tuple_keys::continue_;
     }
 };
 
-#define ftuple_kw(key)                                                  \
+#define ftuple_kw(key)                          \
     return for_tuple_keys::key ## _
 
 #define for_tuple(name, args...)                                        \
-    { [@FORMAT "\n" @]                                                  \
-            VarargsIterator<std::tuple_size<decltype(args)>::value>::iterate \
-            (args, [&](auto &name)                                      \
-             [@ );%NL                                                   \
-             } %NL @]
+    {                                                                   \
+        auto tuple = args;                                              \
+        TupleIterator<std::tuple_size<decltype(tuple)>::value>::iterate(tuple, [&](auto x) \
+        [@ ); } @]
 
 int main()
 {
-    for_tuple(x, std::make_tuple(42, "Hello", 3.14))
-    {
-        if (typeid(x) == typeid(double))
-            ftuple_kw(break);
+    for_tuple(x, std::make_tuple(42, "Hello", 3.14)) {
         std::cout << x << std::endl;
+        if constexpr (std::is_same<const char *, decltype(x)>::value)
+            ftuple_kw(break);
+        else
+            ftuple_kw(continue);
     }
 }
